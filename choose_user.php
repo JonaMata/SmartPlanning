e7ce3f6ef557b7344f4155f8d17c60aeb1f079d4
Leonardo <leonmilo24@gmail.com>
2019-01-24 00:43:18 +0100

    fixed choose_user

--- choose_user.php
+++ choose_user.php
@@ -2,14 +2,17 @@
 $title = 'choose user';
 require('includes/header.php');
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-
+if($_SESSION['loggedIn']){
   require('includes/database.php');
 
   $query = $conn->prepare("SELECT userid FROM association WHERE caretaker_userid = ?");
   $query->bind_param('i', $_SESSION['id']);
   $query->execute();
   $result = $query->get_result();
+
+  if (count($result) == 0){
+    echo "no users found";
+  }
 
   while ($row = $result->fetch_array(MYSQLI_NUM)) {
     error_log(implode('\n', $row));
@@ -21,13 +24,13 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php
   }
 
-  else{
-    die("No users found");
-  }
-
   header("Location: index.php");
 }
-
+else{
+  ?>
+  <span style="font-size:100;">You need to be logged in.
+  <?php
+}
 
 require('includes/footer.php');
 ?>
