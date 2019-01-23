e893ef849f17af621e8bb7ef10344bf1f58ee051
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2019-01-23 20:54:12 +0100

    Revert "Updated register and login (#3)"
    This reverts commit f3adcb7c38b97734eb31898ba5244740e2660832.

--- login.php
+++ login.php
@@ -6,20 +6,13 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   require('includes/database.php');
 
-  $query = $conn->prepare("SELECT id, password FROM users WHERE email = ? and password = ?");
-  $query->bind_param('ss', $_POST['email'], $_POST['password']);
+  $query = $conn->prepare("SELECT id FROM users WHERE username = ? and password = ?");
+  $query->bind_param('ss', $_POST['username'], $_POST['password']);
   $query->execute();
   $result = $query->get_result();
-  if (!$result) {
-        die("User not found");
-  }
-  if ($_POST['password'] !== $result['password']){
-        die("Password does not match");
-  }
-
   if ($row = $result->fetch_array(MYSQLI_NUM)) {
-    $_SESSION['id']=$row['id'];
-    $_SESSION['email']=$_POST['email'];
+    $_SESSION['id']=$row[0];
+    $_SESSION['username']=$_POST['username'];
     $_SESSION['loggedIn']=true;
   }
 
@@ -28,8 +21,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 <form method="POST">
-  e-mail: <input name="email" required><br>
-  Password: <input type="password" name="password" required><br>
+  Username: <input name="username"><br>
+  Password: <input type="password" name="password"><br>
   <button type="submit">Login</button>
 </form>
 
