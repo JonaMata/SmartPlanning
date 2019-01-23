46428c4f12fbf43c132a9a1b6f6f7b210b2aee4e
Leonardo <leonmilo24@gmail.com>
2019-01-24 00:20:13 +0100

    trying to fix links again

--- includes/header.php
+++ includes/header.php
@@ -16,12 +16,7 @@ session_start();
   <body>
     <nav>
       <a href="index.php">Home</a>
-      <a href="
-      <?php
-      if($_SESSION['type'] == "caretaker"){echo "choose_user.php";}
-      else{echo "planning.php";}
-      ?>
-      ">Planning</a>
+      <a href="planning.php">Planning</a>
       <a href="addevent.php">Add-event</a>
       <?php
       if($_SESSION['loggedIn']) {
