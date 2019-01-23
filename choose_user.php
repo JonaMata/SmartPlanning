02ad268c4e3f599aa629886aa0db9da62fc39376
Leonardo <leonmilo24@gmail.com>
2019-01-23 23:53:03 +0100

    Updated links caretaker planning

--- choose_user.php
+++ choose_user.php
@@ -6,7 +6,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   require('includes/database.php');
 
-  $query = $conn->prepare("SELECT userid FROM association WHERE caretakerid = ?");
+  $query = $conn->prepare("SELECT userid FROM association WHERE caretaker_userid = ?");
   $query->bind_param('i', $_SESSION['id']);
   $query->execute();
   $result = $query->get_result();
@@ -14,7 +14,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
   while ($row = $result->fetch_array(MYSQLI_NUM)) {
     error_log(implode('\n', $row));
     ?>
-    <form method="POST">
+    <form method="POST" action="planning.php">
+      <input type="hidden" name="id" value="<?php echo $row[0];?>">
       <button type="submit"><?php echo $row[0];?></button>
     </form>
     <?php
