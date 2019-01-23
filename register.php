ac01ce719a1097374c72472d132d718ce60f5649
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2019-01-23 21:09:55 +0100

    Moved from using username to email

--- register.php
+++ register.php
@@ -5,8 +5,8 @@ require('includes/header.php');
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
   require('includes/database.php');
 
-  $query = $conn->prepare("INSERT INTO users (username, email, password, type) VALUE (?, ?, ?, ?)");
-  $query->bind_param('sssi', $_POST['username'], $_POST['email'], $_POST['password'], intval($_POST['type']));
+  $query = $conn->prepare("INSERT INTO users (email, password, type) VALUE (?, ?, ?, ?)");
+  $query->bind_param('ssi', $_POST['email'], $_POST['password'], intval($_POST['type']));
   $query->execute();
 
   header('Location: login.php');
@@ -18,7 +18,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 <script src="register_check.js"></script>
 
 <form method="POST">
-  Name: <input type="text" name="name" maxlength="100"><br>
+  <!--Name: <input type="text" name="name" maxlength="100"><br>-->
   E-mail: <input type="email" name="email" maxlength="100" required><br>
   Password: <input type="password" id="password" name="password" maxlength="100" onkeyup="check();" required><br>
   Repeat password: <input type="password" id="password-check" name="password-check" maxlength="100" onkeyup="check();" required><br>
