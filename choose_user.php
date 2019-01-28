526dd23e16781a7158da2820289c266b4240a6c4
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2019-01-28 14:01:13 +0100

    Very big merge right here (#9)
    * Added CSS grid
    
    * -Fixed a little css
    
    * fixed css grid
    
    * Secured login and register, changed username to e-mail as identifier
    
    * Updated menu and small correction to login
    
    * Improved security for login and register
    
    * Added change day for planning + small bug fixes
    
    * updated datepicker for planning
    
    * updated datepicker again
    
    * Caretaker can choose user's planning
    
    * Updated links caretaker planning
    
    * trying to fix links
    
    * trying to fix links again
    
    * trying to fix links again again
    
    * fixed choose_user
    
    * Update To Do with planning interface
    
    * Login, settings, and qr-link (#8)
    
    * -Added link below QR-code
    -Changed welcome message
    
    * -Fixed add caretaker link
    
    * -Placed link below qr code
    
    * -Added caretaker category settings view
    
    * -Fixed settings caretaker query
    
    * -Added sql debug to settings
    
    * -Added sql debug to settings
    
    * -Added sql debug to settings
    
    * -Hopefully fixed query
    
    * -Fixed query
    
    * -Fixed query result handling

---
+++ choose_user.php
@@ -0,0 +1,36 @@
+<?php
+$title = 'choose user';
+require('includes/header.php');
+
+if($_SESSION['loggedIn']){
+  require('includes/database.php');
+
+  $query = $conn->prepare("SELECT userid FROM association WHERE caretaker_userid = ?");
+  $query->bind_param('i', $_SESSION['id']);
+  $query->execute();
+  $result = $query->get_result();
+
+  if (count($result) == 0){
+    echo "no users found";
+  }
+
+  while ($row = $result->fetch_array(MYSQLI_NUM)) {
+    error_log(implode('\n', $row));
+    ?>
+    <form method="POST" action="planning.php">
+      <input type="hidden" name="id" value="<?php echo $row[0];?>">
+      <button type="submit"><?php echo $row[0];?></button>
+    </form>
+    <?php
+  }
+
+  header("Location: index.php");
+}
+else{
+  ?>
+  <span style="font-size:100;">You need to be logged in.
+  <?php
+}
+
+require('includes/footer.php');
+?>
