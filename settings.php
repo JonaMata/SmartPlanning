afde3b185555416350440855955828469075ca6a
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2019-01-28 13:59:04 +0100

    Login, settings, and qr-link (#8)
    * -Added link below QR-code
    -Changed welcome message
    
    * -Fixed add caretaker link
    
    * -Placed link below qr code
    
    * -Added caretaker category settings view
    
    * -Fixed settings caretaker query
    
    * -Added sql debug to settings
    
    * -Added sql debug to settings
    
    * -Added sql debug to settings
    
    * -Hopefully fixed query
    
    * -Fixed query
    
    * -Fixed query result handling

---
+++ settings.php
@@ -0,0 +1,53 @@
+<?php
+$title = "Settings";
+require "includes/header.php";
+
+if(!$_SESSION['loggedIn']) die("You need to be logged in.");
+
+require "includes/database.php";
+
+$query = $conn->prepare("SELECT U.email, A.category FROM users U LEFT JOIN association A ON U.id = A.caretaker_userid WHERE A.userid = ?");
+$query->bind_param('i', $_SESSION['id']);
+$query->execute();
+$result = $query->get_result();
+
+?>
+<h1>Settings</h1>
+<hr>
+<br>
+<h2>Caretakers</h2>
+<hr>
+<form>
+    <table>
+        <tr>
+            <th>Caretaker</th>
+            <th>Category</th>
+        </tr>
+        <?php
+        while($row = $result->fetch_array(MYSQLI_NUM)) {
+            ?>
+            <tr>
+                <td><?php echo $row[0]; ?></td>
+                <td>
+                    <select name="<?php echo "caretaker".$row[0]; ?>">
+                        <option value="home" <?php echo ($row[1] == "home" ? "selected" : ""); ?>>Home</option>
+                        <option value="school" <?php echo ($row[1] == "school" ? "selected" : ""); ?>>School</option>
+                        <option value="work" <?php echo ($row[1] == "work" ? "selected" : ""); ?>>Work</option>
+                        <option value="other" <?php echo ($row[1] == "other" ? "selected" : ""); ?>>Other</option>
+                    </select>
+                </td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
+    <button type="submit">Update caretakers</button>
+</form>
+
+
+
+<?php
+
+
+require "includes/footer.php";
+?>
