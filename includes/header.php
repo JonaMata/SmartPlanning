3ebf810e28538391ccc0c13daf4191460b36f502
HyejiYun <newhj2015@gmail.com>
2019-01-28 16:18:07 +0100

    logo

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,7 @@ session_start();
   </head>
   <body>
     <nav>
-      <img src="img/Logo.png" width="50px" height="50px" align="center">
+      <img src="media/Logo.png" width="50px" height="50px" align="center">
       <a href="index.php">Home</a>
       <a href="planning.php">Planning</a>
       <a href="addevent.php">Add-event</a>
