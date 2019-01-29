c96daf427bec43230020c4fd4a4baaa5eb8e5cca
bernieshub <berniesgotmail@gmail.com>
2019-01-29 14:21:31 +0100

    Button class test in index

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ $title = 'Home';
 require('includes/header.php');
 ?>
 
-<div class="item">
+<div class="button">
   Test item 1
 </div>
-<div class="item">
+<div class="button">
   Test item 2
 </div>
 
