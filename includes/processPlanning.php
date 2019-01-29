4e051220c7a0edb78c5811ec8c1770fcc70372fd
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2019-01-30 00:08:13 +0100

    - Added event deviding

--- includes/processPlanning.php
+++ includes/processPlanning.php
@@ -8,49 +8,58 @@ $result = $query->get_result();
 //    if ($row[0] != 0) {
 
 
-        $query = $conn->prepare("SELECT name, description, location, start_time, end_time FROM planning WHERE userid = {$_SESSION['id']} AND date = ? ORDER BY start_time, end_time");
-        $query->bind_param('s', date('Y-m-d'));
-        $query->execute();
+$query = $conn->prepare("SELECT name, description, location, start_time, end_time, fixed, can_next_day FROM planning WHERE userid = {$_SESSION['id']} AND date = ? ORDER BY start_time, end_time");
+$query->bind_param('s', date('Y-m-d'));
+$query->execute();
 
-        $result = $query->get_result();
+$result = $query->get_result();
 
-        $planningItems = array();
 
-        while ($row = $result->fetch_array(MYSQLI_NUM)) {
-            echo "<br>ADDED ROW";
-            $planningItems[] = $row;
+$fixedEvents = array();
+$todayEvents = array();
+$somedayEvents = array();
+$somedayFixedEvents = array();
+
+while ($row = $result->fetch_array(MYSQLI_NUM)) {
+    echo "<br>ADDED ROW";
+    if($row[7] == 1 && $row[6] == 0){
+        $somedayFixedEvents[] = $row;
+    } else if ($row[7] == 1) {
+        $somedayEvents[] = $row;
+    } else if ($row[6] == 0) {
+        $todayEvents[] = $row;
+    } else {
+        $fixedEvents[] = $row;
+    }
+}
+
+$openTimeSlots = array();
+
+
+foreach ($fixedEvents as $value) {
+    $nextStartTime = 0;
+    $endTime = date('Hi');
+
+    echo "<br>EVENT: " . $value[0] . " ENDTIME: " . $endTime;
+    foreach ($fixedEvents as $testValue) {
+        $startTime = date('Hi');
+        echo "<br>EVENT: " . $testValue[0] . " STARTTIME: " . $startTime;
+        if ($startTime > $endTime && $startTime < $nextStartTime) {
+            $nextStartTime = $startTime;
         }
-
-        print_r($planningItems);
-
-        $fixedEvents = array();
-        $openTimeSlots = array();
-
-
-        foreach($fixedEvents as $value) {
-            $nextStartTime = 0;
-            $endTime = date('Hi');
-
-            echo "<br>EVENT: ".$value[0]." ENDTIME: ".$endTime;
-            foreach($fixedEvents as $testValue) {
-                $startTime = date('Hi');
-                echo "<br>EVENT: ".$testValue[0]." STARTTIME: ".$startTime;
-                if($startTime > $endTime && $startTime < $nextStartTime) {
-                    $nextStartTime = $startTime;
-                }
-                if($endTime == $startTime) {
-                    break; //No need to add open time slot since there is an event directly after it.
-                }
-            }
-            if($nextStartTime != 0) {
-                echo "<br>ADDEDTIMESLOT: ".$endTime." DURATION: ".$nextStartTime-$endTime;
-                $openTimeSlots[] = [$endTime, $nextStartTime-$endTime];
-            }
+        if ($endTime == $startTime) {
+            break; //No need to add open time slot since there is an event directly after it.
         }
+    }
+    if ($nextStartTime != 0) {
+        echo "<br>ADDEDTIMESLOT: " . $endTime . " DURATION: " . $nextStartTime - $endTime;
+        $openTimeSlots[] = [$endTime, $nextStartTime - $endTime];
+    }
+}
 
-        foreach($openTimeSlots as $value){
-            print_r($value);
-        }
+foreach ($openTimeSlots as $value) {
+    print_r($value);
+}
 
 
 //        $query = $conn->prepare("INSERT userid, date, processed INTO planning_processed VALUES({$userid}, {date('Y-m-d')}, 1)");
