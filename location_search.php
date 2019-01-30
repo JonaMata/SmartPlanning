a45c3aabca3e8ef206f691456d537b8abe2f36e3
Leonardo <leonmilo24@gmail.com>
2019-01-31 00:54:19 +0100

    test map againnn

--- location_search.php
+++ location_search.php
@@ -3,34 +3,24 @@ $title = 'Home';
 require('includes/header.php');
 ?>
     <script>
-        var marker;
+
         // Initialize and add the map
         function initMap() {
           var pos = {lat: 52.239469, lng: 6.850834};  // university of twente
           var map = new google.maps.Map(document.getElementById('map'), {zoom: 5, center: pos, streetViewControl: false});
+          var marker = new google.maps.Marker({
+              position: pos,
+              map: map,
+              draggable:true,
+              title: 'destination'
+            });
 
           google.maps.event.addListener(map, 'click', function(event) {
             document.getElementById("lat2").value = event.latLng.lat();
             document.getElementById("lng2").value = event.latLng.lng();
 
-            placeMarker(event.latLng);
+            marker.setPosition(event.latLng);
           });
-        }
-        function placeMarker(location) {
-          if(marker == undefined){
-            echo "meow";
-            marker = new google.maps.Marker({
-              position: location,
-              map: map,
-              draggable:true,
-              animation: google.maps.Animation.DROP,
-              title: 'destination'
-            });
-          }
-          else {
-            echo "test";
-            marker.setPosition(location);
-          }
         }
 
 
