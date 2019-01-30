1ec8b3837bffa3dd94ead60350e3a3346f63ac6f
Leonardo <leonmilo24@gmail.com>
2019-01-30 22:39:26 +0100

    Added API key and updated code

--- location_search.php
+++ location_search.php
@@ -5,12 +5,17 @@ require('includes/header.php');
 <script>
         // Initialize and add the map
         function initMap() {
-
             var pos = {lat: 52.14196, lng: 6.51223};  // university of twente
-            var map = new google.maps.Map(document.getElementById('map'), {zoom: 6, center: utwente});
+            var map = new google.maps.Map(document.getElementById('map'), {zoom: 6, center: pos});
             google.maps.event.addListener(map, 'click', function(event) {
-                document.getElementById("inputlat").value = event.latLng.lat();
-                document.getElementById("inputlng").value = event.latLng.lng();
+                document.getElementById("lat2").value = event.latLng.lat();
+                document.getElementById("lng2").value = event.latLng.lng();
+                var marker = new google.maps.Marker({
+                  position: pos,
+                  map: map,
+                  title: 'destination'
+                });
+                marker.setMap(map);
             });
             infoWindow = new google.maps.InfoWindow;
 
@@ -26,6 +31,9 @@ require('includes/header.php');
                 infoWindow.setContent('Location found.');
                 infoWindow.open(map);
                 map.setCenter(pos);
+
+                document.getElementById("lat1").value =setlat;
+                document.getElementById("lng1").value = setlng;
               }, function() {
                 handleLocationError(true, infoWindow, map.getCenter());
               });
@@ -41,55 +49,20 @@ require('includes/header.php');
               'Error: The Geolocation service failed.' :
               'Error: Your browser doesn\'t support geolocation.');
               infoWindow.open(map);
-            }
-
-
-            // code needed for markers _______________________
-
-            <?php
-            require('includes/database.php');
-
-            $query = $conn->prepare("SELECT * FROM locations WHERE id <> ? AND lat IS NOT NULL AND lng IS NOT NULL;");
-            $query->bind_param('i', $_SESSION['id']);
-            $query->execute();
-            $result = $query->get_result();
-
-            for ($i = 0; $i < $query->rowCount(); $i++){
-                $row = $result[$i];
-                ?>
-            var pos<?php echo $i?> = new google.maps.LatLng(<?php echo $row['lat']?>,<?php echo $row['lng']?>);
-            var marker<?php echo $i?> = new google.maps.Marker({position:pos<?php echo $i?>});
-            marker<?php echo $i?>.setMap(map);
-
-            google.maps.event.addListener(marker<?php echo $i?>,'click',function() {
-                document.getElementById("inputlat").value = <?php echo $row['lat']?>;
-                document.getElementById("inputlng").value = <?php echo $row['lng']?>;
-                map.setCenter(marker<?php echo $i?>.getPosition());
-                var infoWindow = new google.maps.InfoWindow({
-                    content:"Title: <?php echo $row['title']?>"
-                });
-                infoWindow.open(map, marker<?php echo $i?>);
-                document.getElementById("saveL").disabled = true;
-                document.getElementById("saveL").style.display = 'none';
-                document.getElementById("deleteL").disabled = false;
-                document.getElementById("deleteL").style.display = 'block';
-            });
-            <?php } ?>
+          }
         }
     </script>
     <!-- API key needs to be updated -->
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=_INSERTKEYHERE_&callback=initMap"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAd8kdXbfNiuxZfyi6UNHH2Rfpck0Pdwfc&callback=initMap"></script>
 
     <div id="map" style="width: 100%; height: 300px"></div><br>
-    <form id="saveL" method="post" action="saveLoc.php">
-        <input type="hidden" id="inputlat" value="">
-        <input type="hidden" id="inputlng" value="">
+    <form id="start" method="post" action="saveLoc.php">
+        <input type="hidden" id="lat1" value="">
+        <input type="hidden" id="lng1" value="">
+        <input type="hidden" id="lat2" value="">
+        <input type="hidden" id="lng2" value="">
+
         <input type="submit" value="save location">
-    </form>
-    <form id="deleteL" method="post" action="deleteLoc.php">
-        <input type="hidden" id="inputlat" value="">
-        <input type="hidden" id="inputlng" value="">
-        <input type="submit" value="deleteLoc">
     </form>
 
 <?php
